a4cd23981795a9f658d8348abb27bc513e0e8720
Mateo <marami26@eafit.edu.co>
2021-08-08 15:26:06 -0500

    add session

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const routes = require('./routes');
 
-require('./mongo');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
 
 const app = express();
 
 app.use(express.json());
+
+app.use(
+  session({
+    secret: 'random',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 },
+    store: MongoStore.create({
+      mongoUrl: 'mongodb://localhost:27017/passport_auth',
+      collection: 'sessions',
+    }),
+  })
+);
 
 app.use(routes);
 
